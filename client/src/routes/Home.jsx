e6a587168c02fa240a0009329bf2238ed9840a8f
Abhishek1438 <abhishekverma1438@gmail.com>
2023-12-02 22:36:57 +0530

    merge final fs

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -16,40 +16,42 @@ import { setUser } from "../features/auth/authSlice.js";
 import { useEffect } from "react";
 
 const Home = () => {
-  const user = useSelector((state) => state.auth.user);
-  const dispatch = useDispatch();
+	const user = useSelector((state) => state.auth.user);
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		const fetchUser = async () => {
 			try {
 				const response = await fetch(
-					"http://localhost:3003/auth/verify",
+					"http://localhost:3000/auth/verify",
 					{
 						method: "GET",
 						credentials: "include",
 					}
 				);
 
-        const data = await response.json();
-        if (data.user) {
-          //set user
-          dispatch(setUser(data.user));
-          console.log("Login successful:", data.user);
-        } else {
-          console.error("Login failed");
-        }
-      } catch (error) {
-        // Handle network errors
-        console.error("Network error:", error);
-      }
-    };
+				const data = await response.json();
+				if (data.user) {
+					//set user
+					dispatch(setUser(data.user));
+					console.log("Login successful:", data.user);
+				} else {
+					console.error("Login failed");
+				}
+			} catch (error) {
+				// Handle network errors
+				console.error("Network error:", error);
+			}
+		};
 
-    fetchUser();
-  }, []);
+		fetchUser();
+	}, []);
 
 	return (
 		<div className={classes.all}>
 			<header>
+				{user && <h1>{user.email}</h1>}
+
 				<div className={classes.HeroBackground}>
 					<div className={classes.headerPart1}>
 						<div className={classes.heroSlogan}>
@@ -79,169 +81,153 @@ const Home = () => {
 						</div>
 					</div>
 					<RbsTabs />
+
+					<div className={classes.p2}>
+						<div className={classes.propertyNumbers}>
+							<div>
+								<p className={classes.colorText}>
+									<span className={classes.faded}>|</span>50k+
+								</p>
+								<p>renters</p>
+							</div>
+							<div>
+								<p className={classes.colorText}>
+									<span className={classes.faded}>|</span>10k+
+								</p>
+								<p>properties</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div className={classes.segment2}>
+				<img src={s2Img} alt="property image" />
+				<div className={classes.segment2Info}>
+					<div className={classes.s2OptionMenu}>
+						<button
+							className={`${classes.s2Options} ${classes.s2Selected}`}
+						>
+							For Tenants
+						</button>
+						<button className={classes.s2Options}>
+							For landlords
+						</button>
+					</div>
+					<div className={classes.s2Info}>
+						<p className={classes.s2Hero}>
+							The ultimate destination for tenants, buyers, and
+							landlords!
+						</p>
+						<p className={classes.s2Desc}>
+							We make it simple and effective to buy a new home,
+							rent a property, or sell your existing one. The best
+							part is that using our services will help you save a
+							tonne of money and time.
+						</p>
+						<a href="/about-us">
+							<button>Learn more </button>
+						</a>
+					</div>
+					<div className={`${classes.s2Info} ${classes.disNone}`}>
+						<p className={classes.s2Hero}>
+							The ultimate destination for tenants, buyers, and
+							landlords!
+						</p>
+						<p className={classes.s2Desc}>
+							We make it simple and effective to buy a new home,
+							rent a property, or sell your existing one. The best
+							part is that using our services will help you save a
+							tonne of money and time.
+						</p>
+						<a href="/about-us">
+							<button>Learn more </button>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div className={classes.segment3}>
+				<div className={classes.s3Info}>
+					<p className={classes.s2Hero}>
+						The modern means of locating a new residence
+					</p>
+					<p className={classes.faded}>
+						With over 10,000 properties available, find the home of
+						your dreams.
+					</p>
+					<div className={classes.s3Icons}>
+						<div>
+							<img src={s3I1} alt="icon1" />
+							<p className={classes.iconText}>6.9%</p>
+							<span
+								className={`${classes.faded} ${classes.iconDesc}`}
+							>
+								Property Return Rate
+							</span>
+						</div>
+						<div>
+							<img src={s3I2} alt="icon2" />
+							<p className={classes.iconText}>6,369</p>
+							<span
+								className={`${classes.faded} ${classes.iconDesc}`}
+							>
+								Property in Sell and Rent
+							</span>
+						</div>
+						<div>
+							<img src={s3I3} alt="icon3" />
+							<p className={classes.iconText}>7,420</p>
+							<span
+								className={`${classes.faded} ${classes.iconDesc}`}
+							>
+								Successful Transactions
+							</span>
+						</div>
+					</div>
+				</div>
+				<img src={s3Img} alt="s3Image" />
+			</div>
+
+			<div className={classes.recommendations}>
+				<div className={classes.recNav}>
+					<a href="/show-properties/sale">
+						<button className={classes.bmp}>
+							Browse more properties
+						</button>
+					</a>
 				</div>
 
-				<div className={classes.HeroBackground}>
-					<div className={classes.headerPart1}>
-						<div className={classes.heroSlogan}>
-							<h1 className={classes.heroText}>
-								Searching for your dream property?
-							</h1>
-							<p className={classes.heroDesc}>
-								Look no further than PropertyWala
-							</p>
-							<p className={classes.heroDesc}>
-								- your gateway to a world of real estate
-								possibilities!
-							</p>
-						</div>
-						<div className={classes.heroPropertyCard}>
-							<PropertyCard
-								property={{
-									price: "price",
-									type: "rent",
-									propertyName: "name",
-									location: "City",
-									area: "area",
-									bedsNum: "beds",
-									bathsNum: "baths",
-								}}
-							/>
-						</div>
+				<p className={`${classes.faded} ${classes.recDesc}`}>
+					Some of the chosen properties in your area.
+				</p>
+
+				<div className={classes.container}>
+					<div className={classes.row}>
+						<p className={classes.s2Hero}>Based on your location</p>
+						<div className={classes.col}></div>
 					</div>
-					<RbsTabs />
+				</div>
+			</div>
 
-        <div className={classes.HeroBackground}>
-          <div className={classes.headerPart1}>
-            <div className={classes.heroSlogan}>
-              <h1 className={classes.heroText}>
-                Searching for your dream property?
-              </h1>
-              <p className={classes.heroDesc}>
-                Look no further than PropertyWala
-              </p>
-              <p className={classes.heroDesc}>
-                - your gateway to a world of real estate possibilities!
-              </p>
-            </div>
-            <div className={classes.heroPropertyCard}>
-              <PropertyCard
-                property={{
-                  price: "price",
-                  type: "rent",
-                  propertyName: "name",
-                  location: "City",
-                  area: "area",
-                  bedsNum: "beds",
-                  bathsNum: "baths",
-                }}
-              />
-            </div>
-          </div>
-          <RbsTabs />
-
-          <div className={classes.p2}>
-            <div className={classes.propertyNumbers}>
-              <div>
-                <p className={classes.colorText}>
-                  <span className={classes.faded}>|</span>50k+
-                </p>
-                <p>renters</p>
-              </div>
-              <div>
-                <p className={classes.colorText}>
-                  <span className={classes.faded}>|</span>10k+
-                </p>
-                <p>properties</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <div className={classes.segment2}>
-        <img src={s2Img} alt="property image" />
-        <div className={classes.segment2Info}>
-          <div className={classes.s2OptionMenu}>
-            <button className={`${classes.s2Options} ${classes.s2Selected}`}>
-              For Tenants
-            </button>
-            <button className={classes.s2Options}>For landlords</button>
-          </div>
-          <div className={classes.s2Info}>
-            <p className={classes.s2Hero}>
-              The ultimate destination for tenants, buyers, and landlords!
-            </p>
-            <p className={classes.s2Desc}>
-              We make it simple and effective to buy a new home, rent a
-              property, or sell your existing one. The best part is that using
-              our services will help you save a tonne of money and time.
-            </p>
-            <a href="/about-us">
-              <button>Learn more </button>
-            </a>
-          </div>
-          <div className={`${classes.s2Info} ${classes.disNone}`}>
-            <p className={classes.s2Hero}>
-              The ultimate destination for tenants, buyers, and landlords!
-            </p>
-            <p className={classes.s2Desc}>
-              We make it simple and effective to buy a new home, rent a
-              property, or sell your existing one. The best part is that using
-              our services will help you save a tonne of money and time.
-            </p>
-            <a href="/about-us">
-              <button>Learn more </button>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div className={classes.segment3}>
-        <div className={classes.s3Info}>
-          <p className={classes.s2Hero}>
-            The modern means of locating a new residence
-          </p>
-          <p className={classes.faded}>
-            With over 10,000 properties available, find the home of your dreams.
-          </p>
-          <div className={classes.s3Icons}>
-            <div>
-              <img src={s3I1} alt="icon1" />
-              <p className={classes.iconText}>6.9%</p>
-              <span className={`${classes.faded} ${classes.iconDesc}`}>
-                Property Return Rate
-              </span>
-            </div>
-            <div>
-              <img src={s3I2} alt="icon2" />
-              <p className={classes.iconText}>6,369</p>
-              <span className={`${classes.faded} ${classes.iconDesc}`}>
-                Property in Sell and Rent
-              </span>
-            </div>
-            <div>
-              <img src={s3I3} alt="icon3" />
-              <p className={classes.iconText}>7,420</p>
-              <span className={`${classes.faded} ${classes.iconDesc}`}>
-                Successful Transactions
-              </span>
-            </div>
-          </div>
-        </div>
-        <img src={s3Img} alt="s3Image" />
-      </div>
-
-      <div className={classes.recommendations}>
-        <div className={classes.recNav}>
-          <a href="/show-properties/sale">
-            <button className={classes.bmp}>Browse more properties</button>
-          </a>
-        </div>
-
-        <p className={`${classes.faded} ${classes.recDesc}`}>
-          Some of the chosen properties in your area.
-        </p>
+			<div className={classes.Testimonial}>
+				<h1>Testimonials</h1>
+				<p className={classes.greyText}>
+					See the opinions of our property managers, landlords, and
+					tenants.
+				</p>
+				<p>
+					` "I use PropertyWala practically every day to hunt for a
+					second home, a vacation condo, or just to browse fantasy
+					homes in different regions. Thank you, PropertyWala, for the
+					enjoyable home searching and analysis! "`
+				</p>
+				<span>
+					<b>Ram,</b>
+					<span className={classes.greyText}>Renter</span>
+				</span>
+				<img src={pIcons} alt="peopleIcons" />
+			</div>
 
 			<div className={classes.clientEmail}>
 				<div>
