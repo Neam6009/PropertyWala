1025216f16bccbfe63c0b79788f5509971696044
Neam6009 <neeleshnani99@gmail.com>
2023-12-05 08:52:08 +0530

    final

--- client/src/components/AdminControlBlogCard.jsx
+++ client/src/components/AdminControlBlogCard.jsx
@@ -1,39 +1,38 @@
 import React from "react";
 import classes from "../assets/Styles/AdminControlBlogCard.module.css";
+import { useState } from "react";
 
 const AdminControlBlogCard = ({ blog }) => {
-	const [deleted, setDeleted] = useState(false);
+  const [deleted, setDeleted] = useState(false);
 
-	const removeBlogHandler = async () => {
-		fetch(`http://localhost:3003/blogs/deleteBlog/${blog._id}`, {
-			method: "POST",
-		}).then(() => setDeleted(true));
-	};
+  const removeBlogHandler = async () => {
+    fetch(`http://localhost:3003/blogs/deleteBlog/${blog._id}`, {
+      method: "POST",
+    }).then(() => setDeleted(true));
+  };
 
-	return (
-		<div>
-			{!deleted && (
-				<div className={classes.blogCardAdminControl}>
-					<div className={classes.blogCardAdminControlChild}>
-						<div>
-							<p className={classes.icon}>{blog.name[0]}</p>
-						</div>
-						<div>
-							<h2>{blog.title}</h2>
-							<div className={classes.card__handle}>
-								<span className={classes.handle}>
-									{blog.date}
-								</span>
-							</div>
-						</div>
-					</div>
-					<div className={classes.blogCardAdminControlButtons}>
-						<button onClick={removeBlogHandler}>Remove Blog</button>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {!deleted && (
+        <div className={classes.blogCardAdminControl}>
+          <div className={classes.blogCardAdminControlChild}>
+            <div>
+              <p className={classes.icon}>{blog.title[0]}</p>
+            </div>
+            <div>
+              <h2>{blog.title}</h2>
+              <div className={classes.card__handle}>
+                <span className={classes.handle}>{blog.date}</span>
+              </div>
+            </div>
+          </div>
+          <div className={classes.blogCardAdminControlButtons}>
+            <button onClick={removeBlogHandler}>Remove Blog</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AdminControlBlogCard;
